f1775fa07cbafd65e9244e855ab1c00ede567d6c
David Scales <davidsscales@gmail.com>
2016-01-20 13:15:03 -0800

    Add biography object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,3 +7,28 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
+
+//define contact info, bio picture url, welcome message. and skills
+var contactInfo = {
+	"mobile" : "360-710-9905",
+	"email" : "davidsscales@gmail.com",
+	"github" : "DavidScales",
+	"location" : "San Francisco"
+};
+var bioPic = "images/veryhandsome.jpg";
+var welcomeMessage = "Welcome!";
+var skills = ["html", "css", "javascript"];
+
+// create biography object using object literal notation
+var bio = {
+	"name" : name,
+	"role" : role,
+	"contactInfo" : contactInfo,
+	"bioPic" : bioPic,
+	"welcomeMessage" : welcomeMessage,
+	"skills" : skills
+};
+
+// TODO
+// append biography object to webpage
+
