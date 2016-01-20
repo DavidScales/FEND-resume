899c4dabde4c95632e7accbd711b7093607521d1
David Scales <davidsscales@gmail.com>
2016-01-20 10:05:42 -0800

    Add code to replace name & role
    Also deleted trivial line in index.html

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// define name and role
+var name = "David Scales";
+var role = "Web Developer";
+// replace name and role in header
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
