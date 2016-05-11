2f013fc89150661586d7715fff15e7bda9162d6d
David Scales <davidsscales@gmail.com>
2016-05-11 15:40:20 +0900

    Update: add site content & change appearance
    Add site content - basically just fleshed out the content of the site, added projects, updated work history, updated education, etc. No major changes in functionality.
    
    Change appearance - just improved the css a bit, altered colors, margins, positions of elements, etc. No major changes in functionality.

--- js/helper.js
+++ js/helper.js
@@ -4,13 +4,13 @@ Helper HTML strings, to be used by resumeBuilder.js to dynamically populate page
 
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<div>%data%</div><hr>';
+var HTMLheaderRole = '<div class=white-text>%data%</div><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="blue-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="blue-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="blue-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><a href="mailto:%data%?&amp;subject=I%20saw%20your%20resume%20today"><span class="blue-text">email</span><span class="white-text">%data%</span><a></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="blue-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="blue-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><a href="https://github.com/%data%" target="_blank"><span class="blue-text">github</span><span class="white-text">%data%</span><a></li>';
 var HTMLblog = '<li class="flex-item"><span class="blue-text">blog</span><span class="white-text">%data%</span></li>';
 var HTMLlocation = '<li class="flex-item"><span class="blue-text">location</span><span class="white-text">%data%</span></li>';
 
@@ -18,36 +18,40 @@ var HTMLbioPic = '<img src="%data%" class="biopic" alt="very handsome face">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="skill-item"><span class="white-text">%data%</span></li>';
 
-var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
+var HTMLworkStart = '<div class="work-entry"></div><br>';
+var HTMLworkEmployer = '<a href="#" target="_blank">%data%';
 var HTMLworkTitle = ' - %data%</a>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkPointsStart = '<br><ul class="points"></ul>';
+var HTMLworkPoint = '<li class="point">%data%</li>';
 
-var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectStart = '<div class="project-entry"></div><br>';
+var HTMLprojectTitle = '<a href="#" target="_blank">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
-var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectDescription = '<p>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectPointsStart = '<br><ul class="points"></ul>';
+var HTMLprojectPoint = '<li class="point">%data%</li>';
 
-var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolStart = '<div class="education-entry"></div><br>';
+var HTMLschoolName = '<a href="#" target="_blank">%data%';
+var HTMLschoolDegree = ', %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<em><br>%data%</em>';
+var HTMLschoolAchievement = '<p>%data%</p>';
 
 // improvised
 var HTMLonlineCourseStart = '<div class="online-course-entry"></div>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
-var HTMLonlineDates = '<div class="date-text">%data%</div><br>';
-var HTMLonlineURL = '<a href="#">%data%</a>';
+var HTMLonlineTitle = '<a href="#">%data%</a>';
+var HTMLonlineSchool = '<p>%data%</p>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
@@ -99,7 +103,9 @@ function initializeMap() {
   var locations;
 
   var mapOptions = {
-    disableDefaultUI: true
+    disableDefaultUI: true,
+    scrollwheel: false,
+    zoomControl: true,
   };
 
   /*
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,15 +17,18 @@ var bio = {
 	'name': 'David Scales',
 	'role': 'Web Developer',
 	'contacts': {
-		'mobile': '360-710-9905',
+		'mobile': '(360) 710-9905',
 		'email': 'davidsscales@gmail.com',
 		'github': 'DavidScales',
+		'githubUrl': 'https://github.com/DavidScales',
 		'location': 'San Francisco',
 		'twitter': 'no twitter'
 	},
 	'biopic': 'images/veryhandsome-500.jpg',
 	'welcomeMessage': 'Welcome!',
-	'skills': ['HTML', 'CSS', 'JavaScript', 'Git and GitHub', 'C', 'Python','Materials Science stuff'],
+	'skills': ['HTML5', 'CSS3', 'JavaScript', 'Git and GitHub',
+	           'Knockout', 'Backbone', 'Bootstrap', 'jQuery', 'Grunt', 'Responsive Design',
+	           'Critical Rendering Path', 'AJAX', 'API\'s', 'Single Page Applications'],
 	display: function() {
 		// format role and name
 		var formattedRole = HTMLheaderRole.replace('%data%', bio.role);
@@ -36,8 +39,8 @@ var bio = {
 
 		// format mobile, email, github, location
 		var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
-		var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
-		var formattedGitHub = HTMLgithub.replace('%data%', bio.contacts.github);
+		var formattedEmail = HTMLemail.replace(/%data%/g, bio.contacts.email);
+		var formattedGitHub = HTMLgithub.replace(/%data%/g, bio.contacts.github);
 		var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
 		// append mobile, email, github, location
 		$('#topContacts').append(formattedMobile)
@@ -48,9 +51,14 @@ var bio = {
 			.append(formattedEmail)
 			.append(formattedGitHub)
 			.append(formattedLocation);
+
+		// append horizontal break to header
+		$('#header').append('<hr>');
+
 		// format and append biopic
-		var formattedBioPic = HTMLbioPic.replace('%data%', bio.biopic);
-		$('#header').append(formattedBioPic);
+		// var formattedBioPic = HTMLbioPic.replace('%data%', bio.biopic);
+		// $('#header').append(formattedBioPic);
+
 		// format and append skills, if available
 		if (bio.skills.length > 0){
 			// append 'skills start'
@@ -75,19 +83,46 @@ WORK
 
 // create work object using object literal notation (JSON)
 var work = {
-	'jobs': [{
-			'employer': 'Puget Sound Naval Shipyard',
-			'title': 'Engineering Aide',
-			'location': 'Bremerton, Washington',
-			'dates': 'Summers 2010, 2011, 2012',
-			'description': 'Assist with organization and standardization of the technical estimates of multiple shipyards for the Navy. Design and construct a Lean supply system to increase supplies accessibility and productivity. Create Lean systems to reduce personnel/equipment location time and improve safety. Interface with engineers and management to report findings. '
-		},
+	'jobs': [
 		{
+			'employer': 'Water Conservation Team',
+			'url': '#',
+			'title': 'Co-founder',
+			'location': 'Belmont, California',
+			'dates': 'Jan - Sept 2015',
+			'points': ['Started and ran a business that provided water saving '+
+			           'services and products to customers in the San Francisco '+
+			           'Bay Area.']
+		},{
 			'employer': 'NASA Glenn Research Center',
+			'url': 'http://www.nasa.gov/centers/glenn/home/index.html',
 			'title': 'Undergraduate Research Intern',
 			'location': 'Cleveland, Ohio',
 			'dates': 'Summer 2014',
-			'description': 'Study high temperature interactions between sand (CMAS) and next generation environmental barrier coating (EBC) materials for silicon-based ceramic-matrix composite (CMC) aircraft engine components. Characterize and evaluate material interactions with SEM, EDS, XRD, and optical microscopy. Work in a team environment and prepare monthly written and oral reports.'
+			'points': ['Analyzed high temperature interactions between sand and '+
+			           'next generation environmental barrier coating materials '+
+			           'with SEM, EDS, XRD, and optical microscopy.',
+			           'Determined the validity of coating material for use in '+
+			           'future research and applications with ceramic-matrix '+
+			           'composite aircraft engine components.',
+			           'Delivered written and oral reports to senior researchers '+
+			           '& performed in a team environment.']
+		},{
+			'employer': 'Puget Sound Naval Shipyard',
+			'url': 'http://www.navy.mil/local/PSNS/',
+			'title': 'Engineering Aide',
+			'location': 'Bremerton, Washington',
+			'dates': 'Summers 2010-2012',
+			'points': ['Assisted in standardizing the technical repair estimation '+
+			           'documents of U.S. Naval shipyards. ',
+			           'Designed and constructed a lean kanban system for local '+
+			           'office supplies, increasing supplies accessibility and '+
+			           'reducing management time.',
+			           'Created a lean office map, reducing personnel & equipment '+
+			           'location time and improving safety policies adherence.',
+			           'Cooperated with professional engineers and administrative '+
+			           'staff to solve problems.',
+			           'Earned four awards for quality work and safety promotion.']
 		}
 	],
 	display: function() {
@@ -99,15 +134,25 @@ var work = {
 
 			// format employer, title, dates, location, description
 			var formattedEmployer = HTMLworkEmployer.replace('%data%', job.employer);
+			formattedEmployer = formattedEmployer.replace('#', job.url);
 			var formattedTitle = HTMLworkTitle.replace('%data%', job.title);
 			var formattedDates = HTMLworkDates.replace('%data%', job.dates);
 			var formattedLocation = HTMLworkLocation.replace('%data%', job.location);
 			var formattedDescription = HTMLworkDescription.replace('%data%', job.description);
-			// append to .work-entry
-			$('.work-entry:last').append(formattedEmployer + formattedTitle)
+
+			// format and append skills, if available
+			if (job.points.length > 0){
+				// append 'points start'
+				$('.work-entry:last').append(formattedEmployer + formattedTitle)
 				.append(formattedDates)
 				.append(formattedLocation)
-				.append(formattedDescription);
+				.append(HTMLworkPointsStart);
+				// for each point, format and append
+				job.points.forEach(function(point){
+					var formattedPoint = HTMLworkPoint.replace('%data%', point);
+					$('.work-entry:last .points').append(formattedPoint);
+				});
+			}
 		});
 	}
 };
@@ -124,16 +169,57 @@ PROJECTS
 // create projects object using object literal notation (JSON)
 var projects = {
 	'projects': [{
-			'title': 'Portfolio',
+			'title': 'Neighborhood Map',
 			'dates': '2016',
-			'description': 'Personal portfolio site',
-			'images': ['http://placehold.it/250x200'],
+			'points': ['Built a responsive single page application (SPA) featuring '+
+			           'a neighborhood map that contains information on local '+
+			           'restaurants.',
+			           'Integrated data from multiple 3rd party API\'s (Yelp API, '+
+			           'Google Maps API) using AJAX.',
+			           'Utilized Knockout JavaScript framework for MV* design,  '+
+			           'Grunt for optimization, and LocalStorage for user data '+
+			           'persistence.'],
+			'url': 'http://davidscales.github.io/portfolio/'
 		},
 		{
-			'title': 'ALU',
-			'dates': '2015',
-			'description': 'Simulated Arithmetic Logic Unit built from logic gates',
-			'images': ['http://placehold.it/250x200'],
+			'title': 'Website Optimization',
+			'dates': '2016',
+			'points': ['Optimized the critical rendering path of a sample site, '+
+			           'increasing PageSpeed score from 27 to 95.',
+			           'Improved scrolling from 13 to 60 frames per second by '+
+			           'eliminating inefficiencies.',
+			           'Reduced function execution three orders of magnitude to '+
+			           'remove a janky animation.'],
+			'url': 'http://davidscales.github.io/portfolio/'
+		},
+		{
+			'title': 'Frogger Clone',
+			'dates': '2016',
+			'points': ['Built an HTML5 Canvas game based on the classic Frogger '+
+			           'with provided engine and graphic assets.',
+			           'Coded player, enemies, and other game entities in '+
+			           'JavaScript’s object-oriented pseudo-classical style.',
+			           'Modified game engine to increase map size and game '+
+			           'difficulty.'],
+			'url': 'http://davidscales.github.io/portfolio/'
+		},
+		{
+			'title': 'Portfolio Site',
+			'dates': '2016',
+			'points': ['Built a responsive portfolio site with HTML5, CSS3, '+
+			           'Bootstrap framework and mobile-first design to showcase '+
+			           'projects.',
+			           'Optimized images with Grunt and responsive image '+
+			           'techniques, improving load time.'],
+			'url': 'http://davidscales.github.io/portfolio/'
+		},
+		{
+			'title': 'Dynamic Resume',
+			'dates': '2016',
+			'points': ['Developed a resume site that dynamically modifies the '+
+			           'DOM with jQuery to display JSON data.',
+			           'This is the site you are looking at!'],
+			'url': 'http://davidscales.github.io/portfolio/'
 		}
 	],
 	display: function() {
@@ -143,15 +229,18 @@ var projects = {
 			$('#projects').append(HTMLprojectStart);
 			// format components
 			var formattedTitle = HTMLprojectTitle.replace('%data%', project.title);
+			formattedTitle = formattedTitle.replace('#', project.url);
 			var formattedDates = HTMLprojectDates.replace('%data%', project.dates);
-			var formattedDescription = HTMLprojectDescription.replace('%data%', project.description);
-			// append component to last '.project-entry'
-			$('.project-entry:last').append(formattedTitle + formattedDates + formattedDescription);
-			// check for images, if present, format and append
-			if (project.images.length > 0) {
-				project.images.forEach(function(image) {
-					var formattedImage = HTMLprojectImage.replace('%data%', image);
-					$('.project-entry:last').append(formattedImage);
+
+			// format and append skills, if available
+			if (project.points.length > 0){
+				// append 'points start'
+				$('.project-entry:last').append(formattedTitle + formattedDates)
+				.append(HTMLprojectPointsStart);
+				// for each point, format and append
+				project.points.forEach(function(point){
+					var formattedPoint = HTMLprojectPoint.replace('%data%', point);
+					$('.project-entry:last .points').append(formattedPoint);
 				});
 			}
 		});
@@ -171,62 +260,94 @@ EDUCATION
 var education = {
 	'schools': [{
 			'name': 'University of Washington',
-			'date': 2014,
+			'date': '2010 - 2014',
 			'location': 'Seattle, Washington',
 			'majors': ['Materials Science and Engineering'],
 			'degree': 'BS',
-			'url': '#'
+			'url': 'http://www.washington.edu/',
+			'achievement': 'Top of my class'
 		},
 		{
 			'name': 'Olympic College',
-			'date': 2010,
+			'date': '2008 - 2010',
 			'location': 'Bremerton, Washington',
 			'majors': ['General Studies'],
 			'degree': 'AA',
-			'url': '#'
+			'url': 'http://www.olympic.edu/',
+			'achievement': 'Completed during high school'
 		}
 	],
 	'onlineCourses': [{
+			'title': 'JavaScript Design Patterns',
+			'school': 'Udacity',
+			'date': 2015,
+			'url': 'https://www.udacity.com/course/javascript-design-patterns--ud989'
+		},
+		{
+			'title': 'Intro to AJAX',
+			'school': 'Udacity',
+			'date': 2015,
+			'url': 'https://www.udacity.com/course/intro-to-ajax--ud110'
+		},
+		{
+			'title': 'Browser Rendering Optimization',
+			'school': 'Udacity',
+			'date': 2015,
+			'url': 'https://www.udacity.com/course/browser-rendering-optimization--ud860'
+		},
+		{
+			'title': 'Website Performance Optimization',
+			'school': 'Udacity',
+			'date': 2015,
+			'url': 'https://www.udacity.com/course/website-performance-optimization--ud884'
+		},
+		{
+			'title': 'Object Oriented JavaScript',
+			'school': 'Udacity',
+			'date': 2015,
+			'url': 'https://www.udacity.com/course/object-oriented-javascript--ud015'
+		},
+		{
 			'title': 'Intro to HTML and CSS',
 			'school': 'Udacity',
 			'date': 2015,
-			'url': '#'
+			'url': 'https://www.udacity.com/course/intro-to-html-and-css--ud304'
 		},
 		{
 			'title': 'Responsive Web Design Fundamentals',
 			'school': 'Udacity',
 			'date': 2015,
-			'url': '#'
+			'url': 'https://www.udacity.com/course/responsive-web-design-fundamentals--ud893'
 		},
 		{
 			'title': 'Responsive Images',
 			'school': 'Udacity',
 			'date': 2016,
-			'url': '#'
+			'url': 'https://www.udacity.com/course/responsive-images--ud882'
 		},
 		{
 			'title': 'Version Control with Git and GitHub',
 			'school': 'Udacity',
 			'date': 2015,
-			'url': '#'
+			'url': 'https://www.udacity.com/course/how-to-use-git-and-github--ud775'
 		},
 		{
 			'title': 'Intro to CS and Programming Using Python',
 			'school': 'MIT',
 			'date': 2014,
-			'url': '#'
+			'url': 'https://www.edx.org/course/introduction-computer-science-mitx-6-00-1x8'
 		},
 		{
 			'title': 'Computation Structures 1: Digital Circuits',
 			'school': 'MIT',
 			'date': 2015,
-			'url': '#'
+			'url': 'https://www.edx.org/course/computation-structures-part-1-digital-mitx-6-004-1x2'
 		},
 		{
 			'title': 'Introduction to Computer Science',
 			'school': 'Harvard',
 			'date': 2015,
-			'url': '#'
+			'url': 'https://www.edx.org/course/introduction-computer-science-harvardx-cs50x'
 		}
 	],
 	display: function() {
@@ -249,6 +370,10 @@ var education = {
 				var formattedMajor = HTMLschoolMajor.replace('%data%', major);
 				$('.education-entry:last').append(formattedMajor);
 			});
+			// format and append achievement
+			var formattedAchievement = HTMLschoolAchievement.replace('%data%', school.achievement);
+			$('.education-entry:last').append(formattedAchievement);
+
 		});
 		// append online course header
 		$('#education').append(HTMLonlineClasses);
@@ -260,10 +385,8 @@ var education = {
 			var formattedTitle = HTMLonlineTitle.replace('%data%', course.title);
 			formattedTitle = formattedTitle.replace('#', course.url);
 			var formattedSchool = HTMLonlineSchool.replace('%data%', course.school);
-			var formattedDates = HTMLonlineDates.replace('%data%', course.date);
-			// append title, school and dates
-			$('.online-course-entry:last').append(formattedTitle + formattedSchool)
-				.append(formattedDates);
+			// append title, school
+			$('.online-course-entry:last').append(formattedTitle + formattedSchool);
 		});
 	}
 };
